12aabe1a5f3a9bd464e90da03fc6c701934bf1e6
Horatius Tang <horatius.tang@gmail.com>
2015-03-19 08:52:37 +0000

    Add .sh for EsperDist

---
+++ EsperDist2/run_myrole.sh
@@ -0,0 +1,3 @@
+#bash run_coordinator.sh -this.id Coordinator -this.port 7123 -qf ./query/queries.txt
+#bash run_worker.sh -this.id Worker01 -this.port 9001 -lqr false
+#bash run_spout.sh -this.id Spout01 -this.port 10001 -event.category A -event.name AJ
