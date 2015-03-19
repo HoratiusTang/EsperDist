8ba6341eae4d96f1c9ba072eba858a454dd82132
HoratiusTang <horatius.tang@gmail.com>
2015-03-19 16:27:29 +0800

    Update make.bat

--- EsperDist2/make.bat
+++ EsperDist2/make.bat
@@ -4,10 +4,10 @@
 
 call .\make_kryonet.bat
 
-javac .\core\dist\esper\core\CoordinatorMain.java -sourcepath .\core;.\io;.\external;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\* 
+javac .\core\dist\esper\core\CoordinatorMain.java -sourcepath .\core;.\io;.\external;.\experiment;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\* 
 
-javac .\core\dist\esper\core\WorkerMain.java -sourcepath .\core;.\io;.\external;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
+javac .\core\dist\esper\core\WorkerMain.java -sourcepath .\core;.\io;.\external;.\experiment;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
 
-javac .\external\dist\esper\external\SpoutMain.java -sourcepath .\core;.\io;.\external;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
+javac .\external\dist\esper\external\SpoutMain.java -sourcepath .\core;.\io;.\external;.\experiment;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
 
-javac .\test\dist\esper\test\TestSimulation3.java -sourcepath .\core;.\io;.\external;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
+javac .\test\dist\esper\test\TestSimulation3.java -sourcepath .\core;.\io;.\external;.\experiment;.\kryonet -d .\bin -classpath .\bin;.\lib\*;.\lib\commons\*;.\lib\kryonet\*;.\lib\sigar-bin\lib\*;.\lib\esper-5.0.0\esper\*;.\lib\esper-5.0.0\esperio-amqp\*;.\lib\esper-5.0.0\esperio-http\*;.\lib\esper-5.0.0\esper-springjms\*;.\lib\esper-5.0.0\esperio-stax\*;.\lib\util\*
