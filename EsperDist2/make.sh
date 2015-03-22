9b8d1b6b3135eeb6fda07356d3eb92242252175e
HoratiusTang <horatius.tang@gmail.com>
2015-03-22 03:18:33 +0000

    Update .sh

--- EsperDist2/make.sh
+++ EsperDist2/make.sh
@@ -1,3 +1,4 @@
+./clean_bin.sh
 ./make_kryonet.sh
 
 javac ./core/dist/esper/core/CoordinatorMain.java -sourcepath ./core:./io:./external:./experiment:./kryonet -d ./bin -classpath ./bin:./lib/*:./lib/commons/*:./lib/kryonet/*:./lib/sigar-bin/lib/*:./lib/esper-5.0.0/esper/*:./lib/esper-5.0.0/esperio-amqp/*:./lib/esper-5.0.0/esperio-http/*:./lib/esper-5.0.0/esper-springjms/*:./lib/esper-5.0.0/esperio-stax/*:./lib/util/* 
