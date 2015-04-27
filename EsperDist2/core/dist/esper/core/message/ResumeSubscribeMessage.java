0ef503c478032bd9b30276566068ac61502c073b
chayanne <chayanne@126.com>
2015-04-27 11:39:40 +0800

    Add serializer for StartSubscribeMessage and ResumeSubscribeMessage

--- EsperDist2/core/dist/esper/core/message/ResumeSubscribeMessage.java
+++ EsperDist2/core/dist/esper/core/message/ResumeSubscribeMessage.java
@@ -3,6 +3,13 @@ package dist.esper.core.message;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.esotericsoftware.kryo.DefaultSerializer;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+@DefaultSerializer(value = ResumeSubscribeMessage.ResumeSubscribeMessageSerializer.class)
 public class ResumeSubscribeMessage extends AbstractMessage {
 	private static final long serialVersionUID = -8126296549165974615L;
 	String streamName;
@@ -34,5 +41,35 @@ public class ResumeSubscribeMessage extends AbstractMessage {
 	}
 	public void setSelectElementNameList(List<String> selectElementName) {
 		this.selectElementNameList = selectElementName;
+	}
+	
+	public static class ResumeSubscribeMessageSerializer extends Serializer<ResumeSubscribeMessage>{
+		@Override
+		public void write(Kryo kryo, Output output, ResumeSubscribeMessage ssm) {
+			kryo.writeObject(output, ssm.getPrimaryType());
+			kryo.writeObject(output, ssm.getSourceId());
+			kryo.writeObject(output, ssm.getStreamName());
+			
+			kryo.writeObject(output, ssm.getSelectElementNameList().size());
+			for(String sn: ssm.getSelectElementNameList()){
+				kryo.writeObject(output, sn);
+			}
+		}
+
+		@Override
+		public ResumeSubscribeMessage read(Kryo kryo, Input input, Class<ResumeSubscribeMessage> type) {
+			ResumeSubscribeMessage rsm = new ResumeSubscribeMessage();
+			rsm.primaryType = kryo.readObject(input, Integer.class);
+			rsm.sourceId = kryo.readObject(input, String.class);
+			rsm.streamName = kryo.readObject(input, String.class);
+			
+			int snCount = kryo.readObject(input, Integer.class);
+			rsm.selectElementNameList = new ArrayList<String>(snCount);
+			for(int i=0; i<snCount; i++){
+				String sn = kryo.readObject(input, String.class);
+				rsm.selectElementNameList.add(sn);
+			}
+			return rsm;
+		}
 	}
 }
--- EsperDist2/core/dist/esper/core/message/StartSubscribeMessage.java
+++ EsperDist2/core/dist/esper/core/message/StartSubscribeMessage.java
@@ -3,6 +3,13 @@ package dist.esper.core.message;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.esotericsoftware.kryo.DefaultSerializer;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+@DefaultSerializer(value = StartSubscribeMessage.StartSubscribeMessageSerializer.class)
 public class StartSubscribeMessage extends AbstractMessage {
 	private static final long serialVersionUID = 6779577168170533005L;
 	String streamName;
@@ -34,5 +41,35 @@ public class StartSubscribeMessage extends AbstractMessage {
 	}
 	public void setSelectElementNameList(List<String> selectElementName) {
 		this.selectElementNameList = selectElementName;
+	}
+	
+	public static class StartSubscribeMessageSerializer extends Serializer<StartSubscribeMessage>{
+		@Override
+		public void write(Kryo kryo, Output output, StartSubscribeMessage ssm) {
+			kryo.writeObject(output, ssm.getPrimaryType());
+			kryo.writeObject(output, ssm.getSourceId());
+			kryo.writeObject(output, ssm.getStreamName());
+			
+			kryo.writeObject(output, ssm.getSelectElementNameList().size());
+			for(String sn: ssm.getSelectElementNameList()){
+				kryo.writeObject(output, sn);
+			}
+		}
+
+		@Override
+		public StartSubscribeMessage read(Kryo kryo, Input input, Class<StartSubscribeMessage> type) {
+			StartSubscribeMessage ssm = new StartSubscribeMessage();
+			ssm.primaryType = kryo.readObject(input, Integer.class);
+			ssm.sourceId = kryo.readObject(input, String.class);
+			ssm.streamName = kryo.readObject(input, String.class);
+			
+			int snCount = kryo.readObject(input, Integer.class);
+			ssm.selectElementNameList = new ArrayList<String>(snCount);
+			for(int i=0; i<snCount; i++){
+				String sn = kryo.readObject(input, String.class);
+				ssm.selectElementNameList.add(sn);
+			}
+			return ssm;
+		}
 	}
 }
