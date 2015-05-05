5a496d5de66cb46b2c181f2087adcb080a1d2884
HoratiusTang <horatius.tang@gmail.com>
2015-05-05 13:26:36 +0000

    Update .sh

--- EsperDist2/run_coordinator.sh
+++ EsperDist2/run_coordinator.sh
@@ -1,2 +1,2 @@
-java -ea -classpath ./bin:./lib/*:./lib/commons/*:./lib/kryonet/*:./lib/sigar-bin/lib/*:./lib/esper-5.0.0/esper/*:./lib/esper-5.0.0/esperio-amqp/*:./lib/esper-5.0.0/esperio-http/*:./lib/esper-5.0.0/esper-springjms/*:./lib/esper-5.0.0/esperio-stax/*:./lib/util/* dist.esper.core.CoordinatorMain $*
+java -Xms2048m -Xmx5120m -ea -classpath ./bin:./lib/*:./lib/commons/*:./lib/kryonet/*:./lib/sigar-bin/lib/*:./lib/esper-5.0.0/esper/*:./lib/esper-5.0.0/esperio-amqp/*:./lib/esper-5.0.0/esperio-http/*:./lib/esper-5.0.0/esper-springjms/*:./lib/esper-5.0.0/esperio-stax/*:./lib/util/* dist.esper.core.CoordinatorMain $*
 
