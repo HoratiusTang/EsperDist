a04123df1fa49db8f38553db9919424368c5ee36
chayanne <chayanne@126.com>
2015-05-29 22:42:39 +0800

    Remove unused classes

--- EsperDist2/core/dist/esper/core/message/StopSubscribeMessage.java
+++ EsperDist2/core/dist/esper/core/message/StopSubscribeMessage.java
@@ -1,25 +1,25 @@
-package dist.esper.core.message;
-
-import java.util.List;
-
-public class StopSubscribeMessage extends AbstractMessage {
-	private static final long serialVersionUID = 4262456315877959677L;
-	String streamName;
-	
-	public StopSubscribeMessage() {
-		super();
-	}
-	public StopSubscribeMessage(String sourceId, String streamName, 
-			String streamEventTypeName, 
-			List<String> selectElementNameList) {
-		super(sourceId);
-		this.primaryType = PrimaryTypes.SUBSCRIBE;
-		this.streamName = streamName;		
-	}
-	public String getStreamName() {
-		return streamName;
-	}
-	public void setStreamName(String streamName) {
-		this.streamName = streamName;
-	}
-}
+//package dist.esper.core.message;
+//
+//import java.util.List;
+//
+//public class StopSubscribeMessage extends AbstractMessage {
+//	private static final long serialVersionUID = 4262456315877959677L;
+//	String streamName;
+//	
+//	public StopSubscribeMessage() {
+//		super();
+//	}
+//	public StopSubscribeMessage(String sourceId, String streamName, 
+//			String streamEventTypeName, 
+//			List<String> selectElementNameList) {
+//		super(sourceId);
+//		this.primaryType = PrimaryTypes.SUBSCRIBE;
+//		this.streamName = streamName;		
+//	}
+//	public String getStreamName() {
+//		return streamName;
+//	}
+//	public void setStreamName(String streamName) {
+//		this.streamName = streamName;
+//	}
+//}
